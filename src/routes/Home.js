62d5b73aee34d824ff462420d1009311d6ce2068
hyemin12 <khnm31@naver.com>
2023-04-13 18:55:19 +0900

    '버튼만들기'

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import MainPost from "../components/MainPost";
 import { data } from "../data";
 import theme from "../components/styles/theme";
 import Intro from "../components/Intro";
+import Button from "../components/Elements/Button";
 
 const Home = () => {
   const posts = data.sort((a, b) => (a.date < b.date ? 1 : -1));
@@ -18,6 +19,10 @@ const Home = () => {
       {mainPosts.map((post) => (
         <MainPost key={post.id} {...post} textOverflow={true} />
       ))}
+      <ButtonRow theme={theme}>
+        <Button text={"Previous Page"} />
+        <Button text={"Next Page"} />
+      </ButtonRow>
     </Layout>
   );
 };
@@ -28,4 +33,9 @@ const IntroWrapper = styled.div`
     display: block;
     padding-top:55px;
 `;
+const ButtonRow = styled.div`
+  display: flex;
+  gap: 1em;
+`;
+const PrevButton = styled(Button)``;
 export default Home;
